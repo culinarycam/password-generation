318f35bce1f1057c18a726bb9fa52190c9cfbf6d
Cameron Singleton <culinarycam98@gmail.com>
2023-05-03 22:33:10 -0400

    added more functional code and if statements and conditionals

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "123456789";
+  var specialChar = "!@#$%^&*";
+
   var charLength = prompt("How many characters long would you like your password to be?")
 
 if (
@@ -9,27 +14,43 @@ if (
   charLength > "128"
 ) {
   alert("Please choose a character length between 8 and 128.");
-  return generatePassword();
 }
+
 // changed car to lowercase letters from cap
-var lowercase = prompt("Would you like to include lowercase letters in you password? Yes = Yes & No = No");
-var uppercase = prompt("Would you like to include uppercase letters in you password? Yes = Yes & No = No");
-var numeric = prompt("Would you like to include numbers in you password? Yes = Yes & No = No");
-var specialchar = prompt("Would you like to include special characters in you password? Yes = Yes & No = No");
+var includeLowercase = confirm("Would you like to include lowercase letters in you password?");
+var includeUppercase = confirm("Would you like to include uppercase letters in you password?");
+var includeNumeric = confirm("Would you like to include numbers in you password?");
+var includeSpecialchar = confirm("Would you like to include special characters in you password?");
 
+var totalCharacters = "";
+var finalPassword = "";
 if (
-  lowercase === "Yes",
-  uppercase === "Yes",
-  numeric === "Yes",
-  specialchar === "Yes"
-) {
-  Math.floor(Math.random() * lowerCase.length, upperCase.length, numeric.length, specialChar.length);
-}
-
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "123456789";
-var specialChar = "!@#$%^&*";
+  includeLowercase === false &&
+  includeUppercase === false &&
+  includeNumeric === false &&
+  includeSpecialchar === false &&
+  ) {
+  alert("Please select atleast one of the selections.");
+} else {
+  if (includeLowercase){
+    totalCharacters += lowerCase
+  }
+  if (includeUppercase) {
+    totalCharacters += upperCase
+  }
+  if (includeNumeric) {
+    totalCharacters += numeric
+  }
+  if (includeSpecialchar) {
+    totalCharacters += specialChar
+  }
+  for(var i = 0; i < charLength; i++) {
+    var randomChar = Math.floor(Math.random() * totalCharacters.length)
+    finalPassword += totalCharacters[randomChar]
+    //Conditional to check to see if password includes all criteria. If all requirements are met, return final password
+  }
+  return finalPassword;
+} 
 }
 
 // Write password to the #password input
